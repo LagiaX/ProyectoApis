3c06d2b71bb4550cfcc55d44c21f98b419fe8511
EdsonCastro <edson.sv@gmail.com>
2015-05-16 10:57:52 +0200

    Funcion Agregar Eliminar Dropbox

--- InicioSesion.py
+++ InicioSesion.py
@@ -1,61 +1,87 @@
-#from IDtwitter import * #Modulo con los tokens de twitter.
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#from IDtwitter import *          #Modulo con los tokens de twitter.
+from HojagoogletoJSON import *
+from AgregarAlumno import *
+from EliminarAlumno import *
+from filetoDropbox import *
 import twitter
 import io
+import os                         #para redireccionar la url
 import json
 from flask import request
-from flask import Flask, render_template
-#from flask.ext.googlemaps import GoogleMaps
-#from flask.ext.googlemaps import Map
-
-app = Flask(__name__)
-#GoogleMaps(app)
-
+from flask import Flask, render_template, redirect
 from crypt import crypt
 
-@app.route("/vermyhojadecalculo", methods=['POST'])
-def vermyhojadecalculo():
-    #aca iria el codigo que redireccionaria iniciando session y mostrando el google hoja de calculo.
-    return render_template('tareas.html')
+app = Flask(__name__)
+
 
 @app.route("/vermygooglecalendar", methods=['POST'])
 def vermygooglecalendar():
     #aca iria el codigo que redireccionaria iniciando session y mostrando el google calendar.
     return render_template('tareas.html')
 
-
 @app.route("/vermytwitter", methods=['POST'])
 def vermytwitter():
     #aca iria el codigo que redireccionaria iniciando session en twitter.
     return render_template('tareas.html')
 
+#Muestra la Hoja de calculo de las notas.
+@app.route("/vernotasgoogle", methods=['POST'])
+def vernotasgogle():    
+    return redirect("https://docs.google.com/spreadsheets/d/1FMVP1auyd-8cTj0QIOElNZ0CB_FATzNnYxvJJmOt4_I/pubhtml",code=302)
+
+#Genera el html de las notas en un una tabla y la muestra.
+@app.route("/vernotas", methods=['POST'])
+def vernotas():    
+    generarTabla()
+    return render_template('tabla.html')
+
+#Agrega un Alumno con sus datos al archivo JSON y lo sube a Dropbox.
+@app.route("/agregarnotaalumno", methods=['POST'])
+def agregarnotaalumno():
+    alumno = request.form['alumno']
+    nota   = request.form['nota'] 
+    estado = request.form['estado']
+    agregaralumnonota(alumno, nota, estado)
+    actulizarNotasDropbox()
+    return render_template('notasexamen.html')
+
+#Elimina el Alumno de el archivo JSON y lo sube al Dropbox.
+#La busqueda es por la primera coincidencia del nombre, importan las mayusculas.
+@app.route("/eliminarnotaalumno", methods=['POST'])
+def eliminarnotaalumno():
+    nombre = request.form['alumno']
+    print nombre
+    print eliminarAlumno(nombre)
+    generarTabla()
+    actulizarNotasDropbox()    
+    return render_template('notasexamen.html')
+
 @app.route("/publicarnotasexamen", methods=['POST'])
 def publicarnotasexamen():
-    #aca iria el codigo de hacedr twitt y mostrar el enlace de la hora de calculo de google.
+    actulizarNotasDropbox()
     return render_template('notasexamen.html')
 
 @app.route("/publicarfechaexamen", methods=['POST'])
 def publicarfechaexamen():
-    #aca iria el codigo para hacer el twitt y postearlo y agregar a google calendar
+    #aca iria el codigo para hacer el twitt y postearlo y agregar a google calendar    
     return render_template('fechaexamen.html')
 
-@app.route("/agregarnotaalumno", methods=['POST'])
-def agregarnotaalumno():
-    #aca iria el codigo de meter linea en hoja de calculo de google
+@app.route("/formularionotasexamen", methods=['POST'])
+def formularionotasexamen():
+    googletoTabla()
     return render_template('notasexamen.html')
 
 @app.route("/formulariofechaexamen", methods=['POST'])
 def formulariofechaexamen():
     return render_template('fechaexamen.html')
 
-@app.route("/formularionotasexamen", methods=['POST'])
-def formularionotasexamen():
-    return render_template('notasexamen.html')
-
 @app.route("/tareas", methods=['POST'])
 def tareas():
     return render_template('tareas.html')
 
-@app.route("/salir", methods=['Post'])
+@app.route("/salir", methods=['POST'])
 def salir():
     htpuser = ''
     htpasswd = ''
@@ -66,12 +92,12 @@ def salir():
 @app.route("/buscar", methods=['POST'])
 def buscar():
     user = request.form['text']
-    termino = pw = request.form['passwd'] 
+    pw   = request.form['passwd'] 
     htpuser = 'Jywn1PBEdYjkg'
     htpasswd = 'Jywn1PBEdYjkg'
     authU = crypt(pw,htpasswd)==htpasswd
     authP = crypt(user,htpuser)==htpuser
-    print authU, authP
+    #print authU, authP
     if (authU == True) and (authP == True):        
         return render_template('tareas.html')
     else:        
@@ -83,3 +109,6 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=False)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)    
